c8e56f758bb3cb9acf002aae69e6702c015b8bda
Ironman <minidolls1030@gmail.com>
2022-11-03 15:44:46 -0500

    fix: refactoring styles

--- src/components/Auth/AuthLogo.tsx
+++ src/components/Auth/AuthLogo.tsx
@@ -1,23 +1,13 @@
-import { styled, Box } from '@mui/material';
 import { UIFlexColumnBox, UIImage } from '../UI';
-
-export const TextBox = styled(Box)(({ theme }) => ({
-  color: '#fff',
-  fontWeight: 700,
-  fontSize: 20,
-  textAlign: 'center',
-  maxWidth: 400,
-  width: '100%',
-  marginTop: '40px',
-}));
+import { UITextBox } from './ui';
 
 export const AuthLogo = () => {
   return (
     <UIFlexColumnBox>
       <UIImage src="images/icons/logo.svg" width={256} height={245} />
-      <TextBox>
+      <UITextBox>
         Maximizing Your Revenue by Providing the Most Detailed Reporting
-      </TextBox>
+      </UITextBox>
     </UIFlexColumnBox>
   );
 };
--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { AppSEO } from '@/components/App';
-import { UIAppLayoutWrapper } from '@/components/UI';
-import { styled, Container, Box } from '@mui/material';
+import { UIAuthContainer, UIAuthLayoutWrapper } from '@/components/UI';
 
 interface Props {
   bg?: string;
@@ -9,30 +8,13 @@ interface Props {
   description?: string;
   children: React.ReactNode | React.ReactNode[];
 }
-const UIAuthContentWrapper = styled(Container)(({ theme }) => ({
-  padding: theme.spacing(0, 6),
-  gap: theme.spacing(3),
-  width: '100%',
-  [theme.breakpoints.down('sm')]: {
-    padding: theme.spacing(6, 6),
-  },
-}));
-
-const UIAuthBackgroundWrapper = styled(Box)(({ theme }) => ({
-  backgroundImage: `url(/images/login/bg.png)`,
-  backgroundSize: 'cover',
-  backgroundRepeat: 'no-repeat',
-  minHeight: '100vh',
-}));
 
 const AuthLayout = (props: Props) => {
   return (
-    <UIAppLayoutWrapper sx={{ background: props.bg }}>
+    <UIAuthLayoutWrapper sx={{ background: props.bg }}>
       <AppSEO title={props.title || ''} description={props.description || ''} />
-      <UIAuthBackgroundWrapper>
-        <UIAuthContentWrapper>{props.children}</UIAuthContentWrapper>
-      </UIAuthBackgroundWrapper>
-    </UIAppLayoutWrapper>
+      <UIAuthContainer>{props.children}</UIAuthContainer>
+    </UIAuthLayoutWrapper>
   );
 };
 
