673002d3964d971184b21c655dfa40a678051a1a
daniel-pit <danielpit1991@gmail.com>
2022-12-15 14:59:10 -0500

    fix: centralize the cards on rewards and location page

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -7,5 +7,6 @@ export {
 export { UIDefaultButton, UIHoverButton } from './Button';
 export { UIContainer } from './Container';
 export { UIImage } from './Image';
+export { UIWrapPanel } from './WrapPanel';
 export { UIAppLayoutWrapper, UIAuthLayoutWrapper } from './Wrapper';
 export { UIDefaultTextField, UISelectBox } from './TextField';
