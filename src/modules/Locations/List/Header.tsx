47b0ebe2c67e79f592cac19f5d1db5db142ff3d3
unknown <danielpit1991@gmail.com>
2023-01-20 18:35:11 -0500

    fix: location list header issue

--- src/modules/Locations/List/Header.tsx
+++ src/modules/Locations/List/Header.tsx
@@ -1,9 +1,6 @@
-import {
-  UIFlexSpaceBox,
-  UIFlexWrapBox,
-  UIDefaultTextField,
-} from '@/components/UI';
-import { Typography, InputAdornment } from '@mui/material';
+import { UIFlexSpaceBox, UIFlexWrapBox, UIImage } from '@/components/UI';
+import { StyledSearchTextField } from './ui';
+import { Divider, Typography, InputAdornment } from '@mui/material';
 import { Search as SearchIcon } from '@mui/icons-material';
 
 interface LocationsHeaderProps {
@@ -17,39 +14,46 @@ export const LocationsHeader = ({
 }: LocationsHeaderProps) => {
   return (
     <>
-      <UIFlexSpaceBox>
-        <Typography
-          sx={{
-            fontWeight: '600',
-            fontSize: '36px',
-            lineHeight: '54px',
-            alignItems: 'center',
-            color: '#89C8C6',
-          }}
-        >
-          Locations
-        </Typography>
-        <UIFlexWrapBox sx={{ gap: '40px' }}>
-          <UIDefaultTextField
-            placeholder="Search"
-            size="small"
+      <UIFlexSpaceBox sx={{ mt: '35px' }}>
+        <UIFlexWrapBox sx={{ alignItems: 'center', gap: '12px' }}>
+          <UIImage src="images/icons/pin.svg" width={25} height={25} />
+          <Typography
             sx={{
-              '.MuiOutlinedInput-root': { width: '160px' },
-              '.Mui-focused': { width: '250px' },
-              input: { color: '#b7b7b7' },
+              fontWeight: '600',
+              fontSize: '36px',
+              lineHeight: '54px',
+              alignItems: 'center',
+              color: '#89C8C6',
             }}
-            value={searchValue}
-            onChange={(e) => onValueChange(e.target.value)}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon sx={{ color: 'rgba(137, 200, 198, 0.4)' }} />
-                </InputAdornment>
-              ),
-            }}
-          />
+          >
+            Locations
+          </Typography>
         </UIFlexWrapBox>
+        <StyledSearchTextField
+          placeholder="Search"
+          size="small"
+          sx={{
+            '.MuiOutlinedInput-root': { width: '160px' },
+            '.Mui-focused': { width: '250px' },
+            input: { color: '#b7b7b7' },
+          }}
+          value={searchValue}
+          onChange={(e) => onValueChange(e.target.value)}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon sx={{ color: '#B7B7B7' }} />
+              </InputAdornment>
+            ),
+          }}
+        />
       </UIFlexSpaceBox>
+      <Divider
+        sx={{
+          mt: '20px',
+          borderColor: 'rgba(137, 200, 198, 0.5)',
+        }}
+      />
     </>
   );
 };
