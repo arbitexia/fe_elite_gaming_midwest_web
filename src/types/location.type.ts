1e19da21fd67fb9590a0fdc04399d2869d7141df
unknown <danielpit1991@gmail.com>
2023-01-20 18:19:39 -0500

    fix: address null issue

--- src/types/location.type.ts
+++ src/types/location.type.ts
@@ -1,8 +1,10 @@
+import { CommonType } from './common.type';
+
 export type LocationType = {
   name: string;
   coordinates: { lat: number; lng: number };
   id: number;
-  location: string;
+  address: CommonType.Address;
   status: boolean;
   type: string;
   urls: string[];
